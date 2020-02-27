4a80b0937ddf01c600150a26fce4236070dac8d1
Mukhamedov Bekzod <U1810203@INHA.local>
2020-02-27 10:58:41 +0500

    Works fine

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ helper.logStart();
 const bot = new TelegramBot(config.TOKEN, {
 	polling: true
 });
+let texts = ["Test.", "Testing..", "Tested..."];
+let counter = 0;
+
+setInterval(() => {
+	bot.editMessageText(texts[counter], {chat_id: -1001077488163, message_id: 256}); // [-1001077488163, 256]);
+	counter++;
+	if (counter >= 3) counter = 0;
+	console.log(counter+1);
+}, 5000)
 
 bot.on('message', msg => {
-	// console.log('Working ', msg);
+	bot.editMessageText("Tests...", {chat_id: -1001077488163, message_id: 256}); // [-1001077488163, 256]);
 	console.log('Working ', msg);
 })
